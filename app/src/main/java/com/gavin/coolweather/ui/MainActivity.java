364162b7049431acf4799fbc63aecfd8575329a4
Gavin <gaobin555@163.com>
2018-12-06 16:50:20 +0800

    完成遍历省市县三级列表的功能“

--- app/src/main/java/com/gavin/coolweather/ui/MainActivity.java
+++ app/src/main/java/com/gavin/coolweather/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.gavin.coolweather;
+package com.gavin.coolweather.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gavin.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
